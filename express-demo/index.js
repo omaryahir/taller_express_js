e36b3dfc7d547e471f21a3ce657ffa275aceb355
omaryahir <1206997+omaryahir@users.noreply.github.com>
2019-09-19 00:28:53 -0500

    UPD: Send a 404 status http

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
 
+const courses = [
+    {id: 1, name: 'course1'},
+    {id: 2, name: 'course2'},
+    {id: 3, name: 'course3'},
+]
+
 /* This methods are included app.get() app.post() app.put() app.delete()*/
 
 app.get('/', (req, res) => {
@@ -8,12 +14,18 @@ app.get('/', (req, res) => {
 });
 
 app.get('/api/courses', (req, res) => {
-    res.send([1, 2, 3, 4]);
+    res.send(courses);
 });
 
 app.get('/api/courses/:id', (req, res) => {
-    res.send(req.params.id);
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) res.status(404).send('The course ' + req.params.id + ' was not found');
+    res.send(course);
 });
+
+
+
+
 
 // http://localhost:5000/api/post/2019/9
 app.get('/api/post/:year/:month', (req, res) => {
