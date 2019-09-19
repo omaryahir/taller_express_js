521d5a768d1a284a3818b38893ab1ca7c42c01cb
omaryahir <1206997+omaryahir@users.noreply.github.com>
2019-09-19 00:23:17 -0500

    ADD: reading params and query string

--- express-demo/index.js
+++ express-demo/index.js
@@ -11,5 +11,20 @@ app.get('/api/courses', (req, res) => {
     res.send([1, 2, 3, 4]);
 });
 
+app.get('/api/courses/:id', (req, res) => {
+    res.send(req.params.id);
+});
+
+// http://localhost:5000/api/post/2019/9
+app.get('/api/post/:year/:month', (req, res) => {
+    res.send(req.params);
+});
+
+// http://localhost:5000/api/post?a=2
+app.get('/api/post/', (req, res) => {
+    res.send(req.query);
+});
+
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log('Listening on port ' + port + '...'));
