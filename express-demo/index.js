7affde6ed7fd07d6cd2fb6c07a8646404d3af54a
omaryahir <1206997+omaryahir@users.noreply.github.com>
2019-09-22 22:45:37 -0500

    ADD: PUT to update courses

--- express-demo/index.js
+++ express-demo/index.js
@@ -28,18 +28,12 @@ app.get('/api/courses/:id', (req, res) => {
 });
 
 app.post('/api/courses', (req, res) => {
-    const schema = {
-        name: Joi.string().min(3).required()
-    };
-
-    const result = Joi.validate(req.body, schema);
-    console.log(result);
-
-    if (result.error) {
-        // 400 Bad Request
-        res.status(400).send(result.error.details[0].message);
+    const { error } = validateCourse(req.body); // result.error
+    if (error) {
+        res.status(400).send(error.details[0].message); // 400 Bad Request
         return;
     }
+
     const course = {
         id: courses.length + 1,
         name: req.body.name 
@@ -48,7 +42,33 @@ app.post('/api/courses', (req, res) => {
     res.send(course);
 });
 
+app.put('/api/courses/:id', (req, res) => {
+    // Look up the course
+    // If not existing, return 404
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) res.status(404).send('The course ' + req.params.id + ' was not found');
 
+    const { error } = validateCourse(req.body); // result.error
+    if (error) {
+        res.status(400).send(error.details[0].message); // 400 Bad Request
+        return;
+    }
+
+    // Update course
+    course.name = req.body.name;
+    // Return the updated
+    res.send(course);
+});
+
+function validateCourse(course) {
+    // Validate
+    // If invalid, return 400 - Bad request
+    const schema = {
+        name: Joi.string().min(3).required()
+    };
+
+    return Joi.validate(course, schema);
+}
 
 // http://localhost:5000/api/post/2019/9
 app.get('/api/post/:year/:month', (req, res) => {
